53262047527f0fc128c45a7f8e96e7e418aa371b
OvasProg <alex.vasyliev.prog@gmail.com>
2023-09-01 16:54:06 +0300

    commit1

---
+++ src/class1.java
@@ -0,0 +1,6 @@
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("My name is Oleksandr. Welcome to my first Java program");
+    }
+}
